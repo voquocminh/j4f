a74123cb7e3e0d27e7d252226629b60b856b0a30
voquocminh <19521857@gm.uit.edu.vn>
2022-07-12 23:37:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running`)
 })
